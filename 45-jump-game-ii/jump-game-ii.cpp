8bd3fc4dbfdaeed79258c9bb4b0504979814ebe3
Ahmed El-Sayed Rabie <125227494+Ahmed543252@users.noreply.github.com>
2026-02-03 13:59:36 +0200

    Time: 63 ms (20.35%) | Memory: 21.5 MB (25.37%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -3,25 +3,36 @@ public:
     int jump(vector<int>& nums) {
         // state of dp ?? idx 
         int n = nums.size();
-        vector dp(n+2,-1);
-        auto calc=[&](auto && calc , int idx)->int{
-            if(idx == n-1){
-                return 0 ;
-            }
-            int &ret = dp[idx];
-            if(~ret)
-            return ret;
-            ret = 1e9 ;
-            for(int i = 1 ; i <= nums[idx] ; i++){
-                if(i+idx<n){
-                    ret = min(ret ,1+ calc(calc,idx+i));
+        vector<int> dp(n+2,1e9);
+        dp[n-1] = 0 ;
+        for(int idx =n-2 ; idx>=0 ; idx-- ){
+            for(int  i =1 ; i<=nums[idx];i++){
+                 if(i+idx<n){
+                    dp[idx] = min(dp[idx],1+dp[idx+i]);
                 }
                 else{
                     break;
                 }
             }
-            return ret;
-        };
-        return calc(calc,0);
+        }
+        // auto calc=[&](auto && calc , int idx)->int{
+        //     if(idx == n-1){
+        //         return 0 ;
+        //     }
+        //     int &ret = dp[idx];
+        //     if(~ret)
+        //     return ret;
+        //     ret = 1e9 ;
+        //     for(int i = 1 ; i <= nums[idx] ; i++){
+        //         if(i+idx<n){
+        //             ret = min(ret ,1+ calc(calc,idx+i));
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     return ret;
+        // };
+        return dp[0];
     }
 };
