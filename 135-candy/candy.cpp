db11defce2965d3ef11d9053821025aec8b81a53
Ahmed El-Sayed Rabie <125227494+Ahmed543252@users.noreply.github.com>
2026-02-06 16:16:05 +0200

    Time: 3 ms (25.82%) | Memory: 22.9 MB (53.29%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        // just want to move from l to r once 
+        // and repeat it from r to l
+        int n = ratings.size();
+        vector<int>have(n,1);
+        for(int i = 1 ; i < n ; i++){
+            if(ratings[i]>ratings[i-1]&& have[i]<=have[i-1]){
+                have[i]+=have[i-1];
+            }
+        }
+        for(int i = n-2 ; i >=0 ; i--){
+            if(ratings[i]>ratings[i+1] ){
+                have[i]= max(have[i] , have[i+1]+1);
+            }
+        }
+        int ans =accumulate(have.begin(),have.end(),0);
+        return ans;
+    }
+};
