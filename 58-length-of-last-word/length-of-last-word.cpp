076dcc955c26d72670be832bd0e2e0f7ab7431aa
Ahmed El-Sayed Rabie <125227494+Ahmed543252@users.noreply.github.com>
2026-02-07 15:18:11 +0200

    Time: 0 ms (100.00%) | Memory: 8.7 MB (91.15%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int r = -1 , l = -1 ;
+        for(int i = s.size()-1 ; i>=0 ; i--){
+            if(s[i]!=' '&& r==-1){
+                r = i;
+            }
+            if(l==-1 && r!=-1 && s[i]==' '){
+                l = i ;
+                break;
+            }
+        }
+        int ans = r - l + 1 ;
+        ans--;
+        return ans ;
+    }
+};
